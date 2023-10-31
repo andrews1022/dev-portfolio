052a208b4989cadbe506e5e3a45ca1d9c61014c3
Andrew Shearer <andrewshearerdev@gmail.com>
2023-10-30 17:41:24 -0700

    created separate files for social icons
    - also updated social-links component to use new icon components

--- src/components/social-links.tsx
+++ src/components/social-links.tsx
@@ -1,5 +1,49 @@
+import { Github } from "@/components/icons/github";
+import { LinkedIn } from "@/components/icons/linkedin";
+import { Twitter } from "@/components/icons/twitter";
+
 const SocialLinks = (): JSX.Element => {
-  return <div>SocialLinks</div>;
+  return (
+    <nav aria-label="Social media links">
+      <ul className="ml-1 mt-8 flex items-center">
+        <li className="mr-5 text-xs">
+          <a
+            className="block hover:text-slate-200"
+            href="https://github.com/bchiang7"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <span className="sr-only">GitHub</span>
+            <Github />
+          </a>
+        </li>
+
+        <li className="mr-5 text-xs">
+          <a
+            className="block hover:text-slate-200"
+            href="https://twitter.com/bchiang7"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <span className="sr-only">Twitter</span>
+            <Twitter />
+          </a>
+        </li>
+
+        <li className="mr-5 text-xs">
+          <a
+            className="block hover:text-slate-200"
+            href="https://www.linkedin.com/in/bchiang7/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <span className="sr-only">LinkedIn</span>
+            <LinkedIn />
+          </a>
+        </li>
+      </ul>
+    </nav>
+  );
 };
 
 export { SocialLinks };
