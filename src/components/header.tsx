7a4146eb08c2d496f59c945da44e5f336d481a98
Andrew Shearer <andrewshearerdev@gmail.com>
2023-10-30 17:24:34 -0700

    added jump links component

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,3 +1,5 @@
+import { JumpLinks } from "./jump-links";
+
 const Header = (): JSX.Element => {
   return (
     <header className="lg:sticky lg:top-0 lg:flex lg:max-h-screen lg:w-1/2 lg:flex-col lg:justify-between lg:py-24">
@@ -14,36 +16,7 @@ const Header = (): JSX.Element => {
           I build accessible, inclusive products and digital experiences for the web.
         </p>
 
-        <nav className="nav hidden lg:block" aria-label="In-page jump links">
-          <ul className="mt-16 w-max">
-            <li>
-              <a className="group flex items-center py-3 active" href="#about">
-                <span className="nav-indicator mr-4 h-px w-8 bg-slate-600 transition-all group-hover:w-16 group-hover:bg-slate-200 group-focus-visible:w-16 group-focus-visible:bg-slate-200 motion-reduce:transition-none"></span>
-                <span className="nav-text text-xs font-bold uppercase tracking-widest text-slate-500 group-hover:text-slate-200 group-focus-visible:text-slate-200">
-                  About
-                </span>
-              </a>
-            </li>
-
-            <li>
-              <a className="group flex items-center py-3" href="#experience">
-                <span className="nav-indicator mr-4 h-px w-8 bg-slate-600 transition-all group-hover:w-16 group-hover:bg-slate-200 group-focus-visible:w-16 group-focus-visible:bg-slate-200 motion-reduce:transition-none"></span>
-                <span className="nav-text text-xs font-bold uppercase tracking-widest text-slate-500 group-hover:text-slate-200 group-focus-visible:text-slate-200">
-                  Experience
-                </span>
-              </a>
-            </li>
-
-            <li>
-              <a className="group flex items-center py-3" href="#projects">
-                <span className="nav-indicator mr-4 h-px w-8 bg-slate-600 transition-all group-hover:w-16 group-hover:bg-slate-200 group-focus-visible:w-16 group-focus-visible:bg-slate-200 motion-reduce:transition-none"></span>
-                <span className="nav-text text-xs font-bold uppercase tracking-widest text-slate-500 group-hover:text-slate-200 group-focus-visible:text-slate-200">
-                  Projects
-                </span>
-              </a>
-            </li>
-          </ul>
-        </nav>
+        <JumpLinks />
       </div>
 
       <ul className="ml-1 mt-8 flex items-center" aria-label="Social media">
