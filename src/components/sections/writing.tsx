970fa36badb24304c963d48a1244593c981c8caf
Andrew Shearer <andrewshearerdev@gmail.com>
2023-11-10 13:47:25 -0800

    updated projects section
    - created arrow-right icon component
    - added thumbnail images for projects
    - updated projects jsx to loop through an array of objects

--- src/components/sections/writing.tsx
+++ src/components/sections/writing.tsx
@@ -44,11 +44,12 @@ const Writing = async (): Promise<JSX.Element> => {
                 <li className="mb-12" key={post.id}>
                   <div className="group relative grid grid-cols-8 gap-4 transition-all sm:items-center sm:gap-8 md:gap-4 lg:hover:!opacity-100 lg:group-hover/list:opacity-50">
                     <div className=" absolute -inset-x-4 -inset-y-4 z-0 hidden rounded-md transition motion-reduce:transition-none lg:-inset-x-6 lg:block lg:group-hover:bg-slate-400/10 lg:group-hover:shadow-[inset_0_1px_0_0_rgba(148,163,184,0.1)]" />
+
                     <Image
                       src={post.cover_image}
                       alt={post.title}
-                      width="200"
-                      height="48"
+                      width={200}
+                      height={48}
                       placeholder="blur"
                       blurDataURL={post.cover_image}
                       className="z-10 col-span-2 rounded border-2 border-slate-200/10 transition group-hover:border-slate-200/30 sm:col-span-2"
