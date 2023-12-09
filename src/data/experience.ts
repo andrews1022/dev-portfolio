518f3d8a56f58362c7d984039b476f44e2105065
Andrew Shearer <andrewshearerdev@gmail.com>
2023-12-08 16:44:40 -0800

    updated description for each experience

--- src/data/experience.ts
+++ src/data/experience.ts
@@ -6,7 +6,8 @@ export const experience: Experience[] = [
     role: "Front End Developer",
     company: "Syatt Media",
     companyUrl: "https://syatt.io/",
-    description: "DESCRIPTION HERE",
+    description:
+      "Pioneered the creation of the company's new website using Jamstack technologies. Specialized in developing client sites with Shopify and contributing to Workarea and VTEX projects while taking a lead role in enhancing React + TypeScript codebases.",
     technologies: [
       "Gatsby",
       "TypeScript",
@@ -37,7 +38,8 @@ export const experience: Experience[] = [
     role: "Front End Developer",
     company: "Massive Media",
     companyUrl: "https://engagemassive.com/",
-    description: "DESCRIPTION HERE",
+    description:
+      "Developed responsive websites utilizing Gatsby, TypeScript, Styled Components, and Contentful, collaborating seamlessly with designers, content strategists, and project managers. Played a key role in WordPress project maintenance and spearheaded internal discussions to enhance the WordPress workflow.",
     technologies: [
       "Gatsby",
       "TypeScript",
@@ -60,7 +62,8 @@ export const experience: Experience[] = [
     role: "Junior Web Developer",
     company: "Opacity Design Group",
     companyUrl: "https://www.opacitydesigngroup.com/",
-    description: "DESCRIPTION HERE",
+    description:
+      "Proficient in constructing responsive websites using WordPress, CSS (SCSS & Less), and JavaScript (ES6+), with a focus on pixel-perfect layouts derived from Illustrator / Photoshop designs. Collaborated on wireframe development, presenting to clients, and actively engaged with clients to address and resolve issues while implementing requested updates.",
     technologies: ["WordPress", "PHP", "SCSS", "LESS", "JavaScript", "Wireframing"],
     links: [
       {
